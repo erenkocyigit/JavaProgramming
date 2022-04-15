12f7054e466c19f7f5d5090eaf4849ee95882dfe
erenkocyigit <m.erenkocyigit@gmail.com>
2022-04-16 01:23:55 +0200

    day38: Inheritance

--- src/day38_Inheritance/carTask/Car.java
+++ src/day38_Inheritance/carTask/Car.java
@@ -2,21 +2,22 @@ package day38_Inheritance.carTask;
 
 public class Car {
 
-    public String brand,model;
+    public String brand, model;
     public int year;
     public double price;
     public String color;
-    public double miles;
+    public int miles;
 
 
-    public void start(){
-        System.out.println(brand + model + " is started");
-    }
-    public void drive(){
-        System.out.println(brand + model + " is driving");
+    public void start() {
+        System.out.println(brand + " " + model + " is started");
     }
 
-    public Car(String brand, String model, int year, double price, String color, double miles) {
+    public void drive() {
+        System.out.println(brand + " " + model + " is driving");
+    }
+
+    public Car(String brand, String model, int year, double price, String color, int miles) {
         this.brand = brand;
         this.model = model;
         this.year = year;
