a4132f53725f4401c878b0f452267546b3a4a4d5
erenkocyigit <m.erenkocyigit@gmail.com>
2022-04-16 01:36:48 +0200

    day38: Inheritance

--- src/day38_Inheritance/carTask/BMW.java
+++ src/day38_Inheritance/carTask/BMW.java
@@ -1,4 +1,13 @@
 package day38_Inheritance.carTask;
 
-public class BMW {
+public class BMW extends Car{
+    public BMW( String model, int year, double price, String color, int miles) {
+        super("BMW", model, year, price, color, miles);
+    }
+    public void breaksDown(){
+        System.out.println(brand + " " + model + " breaks down");
+    }
+    public void racing(){
+        System.out.println(brand + " " + model + " is racing");
+    }
 }
