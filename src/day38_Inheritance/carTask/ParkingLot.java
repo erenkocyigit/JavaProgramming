c73c251f3dcc0addaab721f7190e9606786e66ed
erenkocyigit <m.erenkocyigit@gmail.com>
2022-04-16 02:24:48 +0200

    day38: Inheritance

---
+++ src/day38_Inheritance/carTask/ParkingLot.java
@@ -0,0 +1,16 @@
+package day38_Inheritance.carTask;
+
+import day37_Inheritance.scrumTask.Tester;
+
+public class ParkingLot {
+
+    public static void main(String[] args) {
+
+        Toyota toyota = new Toyota("Corolla",2020,20000,"Gray",123456);
+
+        Tesla tesla = new Tesla("Model S", 2022, 94900,"Red",0);
+
+        BMW bmw = new BMW("X5", 2020,67350, "black",123);
+
+    }
+}
