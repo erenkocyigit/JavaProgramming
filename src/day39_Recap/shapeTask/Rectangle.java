a4f0a0ab03beb7839d59a725b4343811d762b67e
erenkocyigit <m.erenkocyigit@gmail.com>
2022-04-19 01:21:52 +0200

    day39: Recap

--- src/day39_Recap/shapeTask/Rectangle.java
+++ src/day39_Recap/shapeTask/Rectangle.java
@@ -1,6 +1,6 @@
 package day39_Recap.shapeTask;
 
-public class Rectangle  {
+public class Rectangle extends Shape  {
 
     private double length, width;
 
@@ -27,5 +27,12 @@ public class Rectangle  {
         }
         this.width = width;
     }
+
+    public Rectangle( double length, double width) {
+        super("Rectangle");
+        setLength(length);
+        setWidth(width);
+    }
+
 
 }
