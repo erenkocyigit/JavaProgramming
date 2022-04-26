591fd0e5a8fb7cce66312222e23a8654f8a94fbb
erenkocyigit <m.erenkocyigit@gmail.com>
2022-04-26 21:23:33 +0200

    day43 : Abstraction

--- src/day43_Abstraction/employee/EmployeeObjects.java
+++ src/day43_Abstraction/employee/EmployeeObjects.java
@@ -4,6 +4,37 @@ public class EmployeeObjects {
 
     public static void main(String[] args) {
 
+        Tester tester = new Tester("Ali", 30, 'M', 42, "SDET", 145000);
+        Developer developer = new Developer("Alex", 28, 'M', 32, "Web Developer", 120000);
+        Teacher teacher = new Teacher("Angel", 28, 'F', 22, "English Teacher", 65555);
+        Driver driver = new Driver("John",35,'M',42,"Lyft Driver",75000);
+
+        System.out.println(tester);
+        System.out.println(developer);
+        System.out.println(teacher);
+        System.out.println(driver);
+
+        tester.work();
+        tester.sleep();
+        tester.bugReport();
+        tester.eat();
+
+
+        System.out.println("---------------------------------------");
+
+        developer.work();
+        developer.sleep();
+        //  developer.bugReport();
+        developer.eat();
+        developer.unitTest();
+
+        System.out.println("---------------------------------------");
+
+        teacher.work();
+        teacher.sleep();
+        teacher.eat();
+
+        System.out.println("---------------------------------------");
 
 
 
